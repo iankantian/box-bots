21406a0abf9c38e40d9c1b4ba103ff752874445b
Joshua Brown <jbrown@ellation.com>
2017-05-07 11:45:22 -0700

    init commit

---
+++ box-bots.ino
@@ -0,0 +1,190 @@
+/* Code for 2017 Season Box Bots robot control board
+   Arduino Nano clone w/ CH340 USB connected to TB6612 motor driver board
+
+
+   CH1 - Steering
+   CH2 - Throttle
+   CH3 - Weopon
+
+*/
+
+#define   lpwm    3     // pulse width modulation for left motor is pin 3
+#define   lpin1   4     // left control pin one is pin 4
+#define   lpin2   5     // left control pin two is pin 5
+#define   standby 7     // standby pin is 6 - LOW=motor driver off, HIGH=motor driver on for new board
+#define   rpin1   8     // right control pin one is pin 7
+#define   rpin2   9     // right control pin two is pin 8
+#define   rpwm    6     // pulse width modulation for right motor is pin 9 for new board 
+
+#define   forward 0
+#define   reverse 1
+#define   coast   2
+#define   brake   3
+#define   rightMotor 0
+#define   leftMotor 1
+
+#define   in_ch1   10    // input channel one is on pin 10
+#define   in_ch2   11    // input channel two is on pin 11
+#define   in_ch3   12    // input channel three is on pin 12
+
+int ch1; // Steering - Joystick x-axis
+int ch2; // Thottle - Joystick y-axis
+int ch3; // Weapon Switch
+int throttle = 0;
+int spin = 0;
+int rightMotorSpeed = 0;
+int leftMotorSpeed = 0;
+byte  oldDirection = 0; //for troubleshooting stuttering left motor problem
+byte  newDirection = 0;
+
+void motorFunction(byte function, byte motor) {
+  switch (motor) {
+    case leftMotor:
+      switch (function) {
+        case forward:
+          digitalWrite(lpin1, HIGH);
+          digitalWrite(lpin2, LOW);
+          break;
+        case reverse:
+          digitalWrite(lpin1, LOW);
+          digitalWrite(lpin2, HIGH);
+          break;
+
+        case brake:
+          digitalWrite(lpin1, HIGH);
+          digitalWrite(lpin2, HIGH);
+          break;
+
+        default:  // coast condition
+          digitalWrite(lpin1, LOW);
+          digitalWrite(lpin2, LOW);
+          break;
+      }
+      break;
+    case rightMotor:
+      switch (function) {
+        case forward:
+          digitalWrite(rpin1, HIGH);
+          digitalWrite(rpin2, LOW);
+          break;
+
+        case reverse:
+          digitalWrite(rpin1, LOW);
+          digitalWrite(rpin2, HIGH);
+          break;
+
+        case brake:
+          digitalWrite(rpin1, HIGH);
+          digitalWrite(rpin2, HIGH);
+          break;
+
+        default:  // coast condition
+          digitalWrite(rpin1, LOW);
+          digitalWrite(rpin2, LOW);
+      }
+      break;
+    default:
+      break;
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(lpwm, OUTPUT);
+  pinMode(lpin1, OUTPUT);
+  pinMode(lpin2, OUTPUT);
+  pinMode(rpwm, OUTPUT);
+  pinMode(rpin1, OUTPUT);
+  pinMode(rpin2, OUTPUT);
+  pinMode(standby, OUTPUT);
+
+  pinMode(in_ch1, INPUT);       // channel one of RC receiver, x-axis steering
+  pinMode(in_ch2, INPUT);       // channel two of RC receiver, y-axis throttle
+  pinMode(in_ch3, INPUT);       // channel three of RC receiver, switch
+
+  digitalWrite(lpin1, LOW);
+  digitalWrite(lpin2, LOW);
+  digitalWrite(rpin1, LOW);
+  digitalWrite(rpin2, LOW);
+  digitalWrite(standby, HIGH);  // turn on the things
+  
+  Serial.begin(9600); // Pour a bowl of Serial (for debugging)
+}
+
+
+void loop() {
+  // pulsein returning value of 1000 to 2000 (1500 default neutral position)
+  // All Numbers are with transmitter channels in Normal position
+  ch1 = pulseIn(in_ch1, HIGH, 25000); // Steering : 1000 Left, 2000 Right
+  ch2 = pulseIn(in_ch2, HIGH, 25000); // Throttle : 1000 Reverse, 2000 Forward
+  ch3 = pulseIn(in_ch3, HIGH, 25000); // Weapon : 1000 OFF, 2000 full ON
+
+  // handle the case in which the signals time
+  if (ch1 < 800) {
+    ch1 = 1500;
+  }
+  if (ch2 < 800) {
+    ch2 = 1500;
+  }
+  if (ch3 < 800) {
+    ch1 = 1000;
+  }
+
+  ch1 = map(ch1, 1000, 2000, -255, 255); //center over 500
+  ch2 = map(ch2, 1000, 2000, -255, 255); //center over 500
+  ch3 = map(ch3, 1000, 2000, 0, 255);
+
+  if (abs(ch1) < 15) {
+    ch1 = 0;
+  }
+
+  if (abs(ch2) < 15) {
+    ch2 = 0;
+  }
+  spin = -1 * ch1;
+  throttle = -1 * ch2;
+
+  rightMotorSpeed = constrain( throttle + spin, -255, 255);
+  leftMotorSpeed = constrain( throttle - spin, -255, 255 );
+
+
+//  Serial.print("rightMotorSpeed: ");
+//  Serial.print(rightMotorSpeed);
+//  Serial.print("\tleftMotorSpeed: ");
+//  Serial.print(leftMotorSpeed);
+
+    Serial.print(ch1);
+    Serial.print( " ");
+    Serial.print(ch2);
+    Serial.print( " ");
+    Serial.print(ch3);
+
+
+  if (rightMotorSpeed < 0) {  // outside deadband, in reverse
+    //   Serial.print(" Right Back ");
+    motorFunction(reverse, rightMotor);
+  }
+  else {
+    //   Serial.print(" Right Fwd ");
+    motorFunction(forward, rightMotor);
+  }
+  if (leftMotorSpeed < 0) {
+    //   Serial.print(" Left Back ");
+    motorFunction(reverse, leftMotor);
+    newDirection = reverse;
+  }
+  else {
+    newDirection = forward;
+    //   Serial.print(" Left Fwd ");
+    motorFunction(forward, leftMotor);
+  }
+  if (oldDirection != newDirection) {
+    Serial.print("@");
+  }
+  oldDirection = newDirection;
+
+  analogWrite(lpwm, abs(leftMotorSpeed));
+  analogWrite(rpwm, abs(rightMotorSpeed));
+  
+  Serial.println();
+}
